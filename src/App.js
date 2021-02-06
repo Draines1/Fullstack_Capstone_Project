490138bb9e950234d97bb2dc0961bfb8acac3dbe
Draines1 <rainesdarren@gmail.com>
2021-02-05 23:11:11 -0500

    Added Components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Button } from '@material-ui/core';
 
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
           <p>Is this thing on?</p>
-          <Button color="primary">Hello World</Button>
+          <Button color="black">Hello World</Button>
       </header>
     </div>
   );
